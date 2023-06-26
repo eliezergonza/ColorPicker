0ea15c4d891f1266b6ccfbae0434755769015cd4
eliezergonza <eliezergonza@gmail.com>
2023-06-26 00:52:51 -0500

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
-// Write your Color component here
+import { useState } from 'react';
 
+
+//Color Component
+const Color = ({color, setSelectedColor, selectedColor}) => {
+
+  const border = selectedColor === color ? `${color} selected` : color; 
+
+  return  (
+  
+  <div className={border}
+  onClick={() => setSelectedColor(color)}>
+  </div>
+
+  )
+};  //last curly of color component 
+
+
+
+//App Component 
 const App = () => {
+const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
-    </div>
-  );
+
+      <div id="colors-list">
+        <Color color="violet" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+        <Color color="red" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+        <Color color="blue" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+        <Color color="pink" setSelectedColor={setSelectedColor} selectedColor={selectedColor}/>
+      </div>
+
+      <img src="./images/paint.jpg" alt="paint" className="paint"/>
+    </div>  //last div of container
+
+  )
 };
 
 export default App;
